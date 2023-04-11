2c25e33f31fc578b4679f1017f5acbdba4362246
David Luc <luc.ky@me.com>
2023-04-11 18:40:46 -0400

    Initial commit of function that outputs text like a typewriter

---
+++ typewriter.js
@@ -0,0 +1,16 @@
+const typewriter = function(sentence) {
+  let delay = 0;
+
+  for (const char of sentence) {
+    setTimeout(() => {
+      process.stdout.write(char);
+    }, delay);
+    delay += 50;
+  }
+  setTimeout(() => {
+    process.stdout.write("\n");
+  }, delay);
+};
+
+const sentence = "hello there from lighthouse labs";
+typewriter(sentence);
